2c43cb92404964c38573a903394c41e3777644e6
Lauri Pullinen <lauri.pullinen@aalto.fi>
2015-02-04 21:52:28 +0200

    tuned the interface, documentation

--- public/js/urlsubmitter.js
+++ public/js/urlsubmitter.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
-	$("form").submit(function() {
+	// Instead of submitting the form really, get the id with AJAX, then 
+	// display it to the user nicely.
+	$("form").submit(function(event) {
 		var formData = $(this).serialize();
 		$.ajax({
 			url: "/",
 			method: "POST",
 			data: formData,
 			success: function(data) {
-				$("#short-url-container input").val(window.location.protocol + 
+				var container = $("#short-url-container");
+				var urlInput = container.find("input");
+				// Set input value to the new shortened URL
+				urlInput.val(window.location.protocol + 
 					"//" + window.location.host + "/" + data);
-				$("#short-url-container").show();
-				$("#short-url-container input").select();
+				// Show the container (widths don't work for display:none)
+				container.show();
+				// Set the input width to the correct value
+				var available = container.find("#speech-bubble").width();
+				var text = container.find("#text-container").width();
+				urlInput.outerWidth(available - text);
+				// Select the input for easy copying
+				urlInput.select();
 			},
 			error: function() {
 			}
 		});
-		return false;
+		event.preventDefault();
 	});
 });
